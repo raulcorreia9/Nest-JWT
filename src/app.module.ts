9be6301bf9aa83c426930ce1d77ede4b24868ee3
Raul Correia <raulvinicius245@gmail.com>
2022-05-20 11:46:13 -0300

    Add Users e Auth modules.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { UsersController } from './users/users.controller';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UsersModule, AuthModule, MongooseModule.forRoot(process.env.MONGO_URI)],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
